85b28cbb84b7c6d284e446f2c0f7a9ec3ef1c1d8
Guany <1084072690@qq.com>
2024-04-06 13:14:02 +0800

    feat: initial

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { theme } from 'ant-design-vue'
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+
+const locale = ref(zhCN)
+</script>
+
+<template>
+  <a-config-provider
+    :theme="{
+      token: {
+        colorPrimary: '#3498db',
+      },
+      algorithm: isDark ? theme.darkAlgorithm : theme.defaultAlgorithm,
+    }"
+    :locale="locale"
+  >
+    <RouterView />
+  </a-config-provider>
+</template>
